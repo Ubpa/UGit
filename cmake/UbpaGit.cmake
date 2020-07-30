feed9abf556b9708ce8751766ef077885ed7a0b2
Ubpa <641614112@qq.com>
2020-07-30 09:30:02 +0800

    recover

---
+++ cmake/UbpaGit.cmake
@@ -0,0 +1,34 @@
+# ----------------------------------------------------------------------------
+#
+# Ubpa_InitGit()
+# - find git [required]
+#
+# ----------------------------------------------------------------------------
+#
+# Ubpa_UpdateSubModule()
+# - update submodule
+#
+# ----------------------------------------------------------------------------
+
+message(STATUS "include UbpaGit.cmake")
+
+macro(Ubpa_InitGit)
+	message(STATUS "----------")
+	find_package(Git REQUIRED)
+	message(STATUS "GIT_FOUND: ${GIT_FOUND}")
+	message(STATUS "GIT_EXECUTABLE: ${GIT_EXECUTABLE}")
+	message(STATUS "GIT_VERSION_STRING: ${GIT_VERSION_STRING}")
+endmacro()
+
+function(Ubpa_UpdateSubModule)
+	if(NOT GIT_FOUND)
+		message(FATAL_ERROR "you should call Ubpa_InitGit() before calling Ubpa_UpdateSubModule()")
+	endif()
+	execute_process(
+		COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+		#OUTPUT_VARIABLE out
+		#OUTPUT_STRIP_TRAILING_WHITESPACE
+		#ERROR_QUIET
+		WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+	)
+endfunction()
